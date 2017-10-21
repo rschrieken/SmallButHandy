99c9cf2375221d990cf2c3654c286882724951ff
rschrieken <rene.schrieken@gmail.com>
2017-10-21 18:04:03 +0200

    used SE notify instead of own

--- CheckForExistingCorA.user.js
+++ CheckForExistingCorA.user.js
@@ -1,7 +1,7 @@
  // ==UserScript==
 // @name         Check if comment and answer exist
 // @namespace    http://meta.stackexchange.com/users/158100/rene
-// @version      0.1
+// @version      0.2
 // @description  Show a message if the answer or comment in the hash of the url doesn't exist
 // @author       rene
 // @match        *://*.stackexchange.com/questions/*
@@ -14,23 +14,20 @@
 // @grant        none
 // ==/UserScript==
 
-(function ($, window) {
+(function ($, window, StackExchange) {
 
-    var hash = $(window.location).attr('hash'),
-        msg = $('<div></div>')
-                    .css('background-color','#CCFFCC')
-                    .css('align','center')
-                    .css('padding', '10px')
-                    .css('text-align', 'center')
-                    .hide();
+    var hash = $(window.location).attr('hash');
 
     function showMessage(jqsel, text) {
         if (jqsel.length === 0) {
-            $('div#header').append(
-                msg
-                .text('The ' + text + ' is not found. It may have been deleted')
-                .fadeIn(2000)
-                .fadeOut(5000));
+            // wait for SE to be ready
+            StackExchange.initialized.done(function() {
+                var classid = 'missing-post-or-comment';
+                // put a notify on top
+                StackExchange.notify.show('The ' + text + ' is not found. It may have been deleted', classid);
+                // remove it automagically after 5 seconds
+                setTimeout(function () { StackExchange.notify.close(classid);}, 5000);
+            });
         }
     }
 
@@ -40,9 +37,9 @@
             ids = cleanHash.substring(7).split('_');
             showMessage($('div#comments-' + ids[1]).find('tr#comment-'+ ids[0]), 'comment');
         } else {
-            if (Number.parseInt(cleanHash, 10) !== NaN) {
+            if (!isNaN(Number.parseInt(cleanHash, 10))) {
                 showMessage($('div#answer-' + cleanHash), 'answer');
             }
         }
     }
-}($ || unsafeWindow.$, window || unsafeWindow ));
+}($ || unsafeWindow.$, window || unsafeWindow, StackExchange || unsafeWindow.StackExchange ));
