54dc6e1ffd6221037fbecc33ad0c6ea6183943ce
rschrieken <rene.schrieken@gmail.com>
2017-10-21 22:36:55 +0200

    removed jQuery dependency

--- CheckForExistingCorA.user.js
+++ CheckForExistingCorA.user.js
@@ -1,7 +1,7 @@
  // ==UserScript==
 // @name         Check if comment and answer exist
 // @namespace    http://meta.stackexchange.com/users/158100/rene
-// @version      0.2
+// @version      0.3
 // @description  Show a message if the answer or comment in the hash of the url doesn't exist
 // @author       rene
 // @match        *://*.stackexchange.com/questions/*
@@ -14,12 +14,12 @@
 // @grant        none
 // ==/UserScript==
 
-(function ($, window, StackExchange) {
+(function (document, window, StackExchange) {
 
-    var hash = $(window.location).attr('hash');
+    var hash = (window.location && window.location.hash)?window.location.hash:'';
 
     function showMessage(jqsel, text) {
-        if (jqsel.length === 0) {
+        if (jqsel === null) {
             // wait for SE to be ready
             StackExchange.initialized.done(function() {
                 var classid = 'missing-post-or-comment';
@@ -31,15 +31,25 @@
         }
     }
 
-    if (hash.length > 0) {
-        cleanHash = hash.substring(1);
-        if (cleanHash.indexOf('comment') === 0) {
-            ids = cleanHash.substring(7).split('_');
-            showMessage($('div#comments-' + ids[1]).find('tr#comment-'+ ids[0]), 'comment');
-        } else {
-            if (!isNaN(Number.parseInt(cleanHash, 10))) {
-                showMessage($('div#answer-' + cleanHash), 'answer');
+    StackExchange.ready( function() {
+        if (hash.length > 0) {
+            cleanHash = hash.substring(1);
+            if (cleanHash.indexOf('comment') === 0) {
+                ids = cleanHash.substring(7).split('_');
+                var lnk = document.getElementById('comments-link-' + ids[1]);
+                if (lnk !== null) {
+                    StackExchange.comments.loadAll(lnk).done(function() {
+                        showMessage(document.getElementById('comment-'+ ids[0]), 'comment');
+                    });
+                } else {
+                    showMessage(document.getElementById('comment-'+ ids[0]), 'comment');
+                }
+            } else {
+                if (!isNaN(Number.parseInt(cleanHash, 10))) {
+                    showMessage(document.getElementById('answer-' + cleanHash), 'answer');
+                }
             }
         }
-    }
-}($ || unsafeWindow.$, window || unsafeWindow, StackExchange || unsafeWindow.StackExchange ));
+    });
+
+}(document || unsafeWindow.document, window || unsafeWindow, StackExchange || unsafeWindow.StackExchange ));
