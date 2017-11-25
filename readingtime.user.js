c6a84eda9fe49a49f93e2e429e8757c84b2ca0e1
rschrieken <rene.schrieken@gmail.com>
2017-11-25 12:27:20 +0100

    better word count , added image count
    and implemented surpressing the read time when 1 minute

--- readingtime.user.js
+++ readingtime.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Add reading time
 // @namespace   https://meta.stackexchange.com/users/158100/rene
-// @version      0.2
+// @version      0.3
 // @description  adds reading time to each question and answer
 // @author       rene
 // @match        https://*.stackoverflow.com/questions/*
@@ -18,29 +18,56 @@
     'use strict';
 
     function numberOfWords(text) {
-        // let's do the simplest that can work first
-        return text.split(' ').length;
+        var wordsregex = /\b(\w+)\b/g,
+            match,
+            tot = 0;
+        while ((match = wordsregex.exec(text)) !== null) {
+            // This is necessary to avoid infinite loops with zero-width matches, as found on https://regex101.com/r/byXy4H/1/
+            if (match.index === wordsregex.lastIndex) {
+                regex.lastIndex++;
+            }
+            tot++;
+        }
+        // console.log('words: ' + tot);
+        return tot;
+    }
+
+    function numberOfImages($post) {
+        var tagCount = 0;
+        ['img', 'svg'].forEach(function(elem) {
+            tagCount += $post.find(elem).length;
+        });
+        // console.log('images: ' + tagCount);
+        return tagCount;
     }
 
     function readingTimeInMinutes(wordCount, imageCount) {
         // words is 275 per minute, no idea how images influence that
-        var wordMinutes = ((wordCount || 0) / 275),
-            imageMinutes = (imageCount || 0);
+        const WPM = 275;
+        const SecondsPerImage = 15;
+        var wordMinutes = ((wordCount || 0) / WPM),
+            imageMinutes = ((imageCount || 0) * SecondsPerImage) / 60;
 
-        // add and round (maybe ceil()?)
+        // add and ceil()
         return Math.ceil(wordMinutes + imageMinutes, 1);
     }
 
     $('.post-text').each(function () {
-        var minutes = readingTimeInMinutes(numberOfWords(this.textContent));
+        var words = numberOfWords(this.textContent);
+        var images = numberOfImages($(this));
+        var minutes = readingTimeInMinutes(words, images);
         // giant S
         var minutelabel = minutes === 1 ? "minute": "minutes";
-        $(this)
-            .prepend(
+        if (minutes > 1) {
+            $(this)
+                .prepend(
                 $('<span>')
-                    .text(minutes + ' min read')
-                    .prop('title', minutes + ' ' + minutelabel +' reading time')
-                    .addClass('label-key')
-        );
+                .text(minutes + ' min read')
+                .prop('title', minutes + ' ' + minutelabel +' reading time for ' + words + ' words and ' + images + ' images')
+                .addClass('label-key')
+            );
+        } else {
+            console.log('no reading time shown for ' + minutes + ' ' + minutelabel +' reading time for ' + words + ' words and ' + images + ' images');
+        }
     });
 })($);
