e41a6e4c54f52dc90d0d49e4b9381187df5ec890
rschrieken <rene.schrieken@gmail.com>
2023-02-19 17:29:55 +0100

    filtering on non-deleted messages and heatmap

--- improveChatAdminRecentFlags.user.js
+++ improveChatAdminRecentFlags.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ImproveChatAdminRecentFlags
 // @namespace    https://meta.stackexchange.com/users/158100/rene
-// @version      0.2
+// @version      0.3
 // @description  Sorting for Chat Admin Recent flags
 // @author       rene
 // @match        https://chat.stackexchange.com/admin/recent-flags
@@ -13,16 +13,37 @@
 
 (function() {
     'use strict';
+    const idColumn = 0;
     const flagdateColumn = 5;
+    const roomColumn = 6;
+    const authorColumn = 7;
     const table = document.getElementsByTagName('table')[0];
     const tbody = document.getElementsByTagName('tbody')[0];
     const theadRow = tbody.firstChild;
     const thead = document.createElement('thead');
     const filter = {};
 
+    function createDeletedFilter(currentIndex, th) {
+        if (currentIndex === idColumn) {
+            const check = document.createElement('input');
+            check.setAttribute('type', 'checkbox');
+            check.setAttribute('title', 'only non-deleted');
+            th.appendChild(check);
+            check.addEventListener('change', (ev) => {
+                if (ev.target.checked) {
+                    filter[currentIndex] = { showNonDeleted: true };
+                } else if (filter[currentIndex].showNonDeleted) {
+                    delete filter[currentIndex]
+                }
+                applyFilter();
+            });
+        }
+    }
+
     function createFilterColumn(currentIndex) {
         const th = document.createElement('th');
         const input = document.createElement('input');
+        input.setAttribute('placeholder', 'filter');
         th.appendChild(input);
         input.addEventListener('keyup', (ev) => {
             if (ev.target.value) {
@@ -32,6 +53,7 @@
             }
             applyFilter();
         });
+        createDeletedFilter(currentIndex, th);
         return th;
     }
 
@@ -68,10 +90,62 @@
         }
     }
 
+    function applyConditionalFormatting() {
+        const cols = [roomColumn, authorColumn];
+        for(const col of cols) {
+            const valueCounts = {};
+            let total = 0;
+            for(const row of tbody.childNodes) {
+                const td = row.childNodes[col];
+                if (td) {
+                    if (valueCounts[td.textContent]) {
+                        if (valueCounts[td.textContent] === 1 ){
+                            total = total + 1;
+                        }
+                        valueCounts[td.textContent] = valueCounts[td.textContent] + 1;
+                    } else {
+                        valueCounts[td.textContent] = 1;
+                    }
+                }
+            }
+            for(const row of tbody.childNodes) {
+                const td = row.childNodes[col];
+                if (td) {
+                    var count = valueCounts[td.textContent];
+                    if (count > 1 && total > 1) {
+                        var perc = Math.round(count / total * 100);
+                        var rp = (100-perc);
+                        var yp = (100-perc) / 2;
+                        var wp = (100-perc) / 4;
+                        td.style.background = `linear-gradient(to right, white ${wp}%, yellow ${yp}%, red ${rp}%)`
+                    }
+                }
+            }
+        }
+    }
+
+    function applyFilterForColumn(key, col) {
+        if (key === idColumn.toString() && filter[key].showNonDeleted) {
+            const link = col.firstChild;
+            if (link.getAttribute('href').indexOf('/history') !== -1) {
+                return true;
+            }
+        } else {
+            let sourceText;
+            if (col.hasChildNodes()) {
+                sourceText = col.innerHTML;
+            } else {
+                sourceText = col.textContent;
+            }
+            return sourceText.indexOf(filter[key]) === -1;
+        }
+        return false;
+    }
+
     function applyFilterRow(row) {
         row.classList.remove('filtered');
         for(const key in filter) {
-            if (row.childNodes[key].textContent.indexOf(filter[key]) === -1 ) {
+            if (applyFilterForColumn(key, row.childNodes[key])) {
                 row.classList.add('filtered');
             }
         }
@@ -171,8 +245,8 @@
         }
         addTableHead();
         setSort(0, 'desc');
-
         fixDates();
+        applyConditionalFormatting();
     }
 
     function addTitle() {
@@ -192,28 +266,54 @@
       th:hover {
         cursor: pointer;
       }
+      thead tr th {
+        border: 1px solid #F6F6F6;
+      }
       thead tr th:nth-child(6) {
         width: 150px;
       }
       th.js-sort-asc::after {
          content: '▲';
+         color: blue;
+         padding-left: 3px;
       }
       th.js-sort-desc::after {
          content: '▼';
+         color: blue;
+         padding-left: 3px;
       }
       td {
-        border: 1px solid grey;
+        border: 1px solid #F6F6F6;
         margin: 2px;
         padding: 1px;
         padding-left: 5px;
         padding-right: 5px;
         border-collapse: collapse;
+        cursor: default;
       }
       .filtered {
          display: none;
       }
       thead tr th input {
         width: 100%;
+        border: none
+      }
+      thead tr th:first-child {
+        text-align:left;
+      }
+      thead tr th:first-child input:first-child {
+        width: 70%;
+        display:inline;
+        border: none
+      }
+      thead tr th:first-child input:not(:first-child) {
+        width: 20%;
+        display:inline;
+        border: none
+      }
+      thead tr th input::placeholder {
+        color:light-grey;
+        opacity: 0.5;
       }
     `;
     }
